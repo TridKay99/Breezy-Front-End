392d75542d35f1df82a52b03e885c5cd1e893891
Tristan Kay <tridkay90@gmail.com>
2019-07-10 10:38:52 +1000

    created profile page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Maps from './MapContainer'
 import '../App.css'
-import Clothes from './trialClothes'
+import Clothes from './Clothes'
 
 const Home = () => {
 return (
---
+++ src/components/Profile.js
@@ -0,0 +1,38 @@
+// This page will be the userpage.
+import React from 'react'
+import axios from 'axios'
+
+class Profile extends React.Component {
+  state = {
+    user: null
+  };
+  
+  async componentDidMount() {
+    const response = await axios('FIND USER BY _ID URL')
+    // Should be response.data
+    // After confirming its the right pathway for the data
+    this.setState({
+      user: response.data
+    })
+  }
+
+  // In this render we ideally want basic information(Name, DOB, Date create,(IMAGE?, if no image, default image?))
+  // A link or a side box with the users pending/history of jobs thats been created. "DRY CLEANING COMING  UP"
+
+  render() {
+    return(
+      <h1>hello</h1>
+    )
+    // const { user } = this.state
+    // Assume user doesnt exist in first render or render user profile. Other wise this will crash.
+    // if (!user) {
+    //   return null
+    // } else {
+    //   return // information for User profile page. 
+      // name, age, dob - links to pending orders.
+    // }
+  }
+}
+
+
+export default Profile
