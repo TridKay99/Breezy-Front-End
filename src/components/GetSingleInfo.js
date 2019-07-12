eca235718869a26ce89b17d0beaac9021205c00f
Tristan Kay <tridkay90@gmail.com>
2019-07-12 13:04:04 +1000

    added user information and links back to admin/full users list

--- src/components/GetSingleInfo.js
+++ src/components/GetSingleInfo.js
@@ -1,38 +1,29 @@
 import React from 'react'
 
 class GetSingleInfo extends React.Component {
-  state = {
-    user: null
-  }
-  
   componentDidMount() {
-    console.log('userinfo did mount')
-  }
-
-  async componentDidUpdate() {
-      console.log('USERINFO DID UPDATE')
-      if (this.props.users) {
-      const {user} = this.props.users;
+    if (this.props.user) {
+      const { user } = this.props.user;
       fetch(user)
       .then(resp => resp.json())
       .then(json => {
-        this.setState({
-          user: json
-        });
+        console.log(json);
       })
     }
   }
-
   render() {
-    if (this.state.user) {
-      return (
-      <div>
-          <h3>New Update!!!</h3>
-          {/* <h4>{email}'s Page</h4> */}
-      </div>
-      );
-    }
+    console.log('HELLO')
+    if (!this.props.user) {
     return null
+    } else {
+      return (
+        <div className="userContainer">
+          <p>THIS WILL BE USERS NAME</p>
+          <p>Email: {this.props.user.user.email}</p>
+          <p>THIS WILL BE JOBS</p>
+        </div>
+      )
+    }
   }
 }
 
