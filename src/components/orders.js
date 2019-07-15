2092bf3e947619eb18b906b485f108c8f2a78918
alexanderb444 <bishop.alexander@gmail.com>
2019-07-15 15:50:11 +1000

    updated orders page to include multiple items

--- src/components/orders.js
+++ src/components/orders.js
@@ -3,20 +3,107 @@ import axios from 'axios'
 
 const Order = () => {
 
+    const clothes1 = []
+
+
     const [formData, setFormData] = useState({
         clothes: [],
         location: '',
         price: ''
     })
 
+    const HeavyCoat = {
+        name: "Heavy Coat",
+        price: 24.20,
+        quanity: 0,
+        id: "5d281b5a95643703ba61b8d1"
+    }
+
+    const Trousers = {
+        name: "Trousers",
+        price: 12.10,
+        quanity: 0,
+        id: "5d281b8595643703ba61b8d2"
+    }
+
+    const Jacket = {
+        name: "Jacket",
+        price: 13.10,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d3"
+    }
+
+    const Dress = {
+        name: "Dress",
+        price: 22,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d4"
+    }
+
+    const Skirt = {
+        name: "Skirt",
+        price: 12.10,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d5"
+    }
+
+    const Blouse = {
+        name: "Blouse",
+        price: 12.10,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d6"
+    }
+
+    const Shirt = {
+        name: "Shirt",
+        price: 5,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d7"
+    }
+
+    const Tie = {
+        name: "Tie",
+        price: 11,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d8"
+    }
+
+    const Scarves = {
+        name: "Tie",
+        price: 12.20,
+        quanity: 0,
+        id: "5d281b8d95643703ba61b8d9"
+    }
+
     const {clothes, location, price} = formData
+
+    let ids = clothes.map(a => a.id);
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
 
-    const myFunction = () => {
-        clothes.push("5d281b5a95643703ba61b8d1")
-        console.log(clothes)
+    const HeavyCoatadd = () => {
+        clothes1.push(HeavyCoat)
+        clothes.push(HeavyCoat.id)
+        let result = clothes1.map(a => a.price);
+        let added = result.reduce((a, b) => a + b, 0)
     }
+
+    const Trousersadd = () => {
+        clothes1.push(Trousers)
+        clothes.push(Trousers.id)
+        let result = clothes1.map(a => a.price);
+        let added = result.reduce((a, b) => a + b, 0)
+    }
+
+    const Jacketadd = () => {
+        clothes1.push(Jacket)
+        clothes.push(Jacket.id)
+        let result = clothes1.map(a => a.price);
+        let added = result.reduce((a, b) => a + b, 0)
+        console.log(clothes1)
+        console.log(result)
+        console.log(added)
+     }
 
     const onSubmit = async e => {
         e.preventDefault()
@@ -30,12 +117,13 @@ const Order = () => {
                 const config = {
                     headers: {
                         'Content-Type': 'application/json',
-                        'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWQxZDg4NzUyY2I2ZTUwYTIwYzliN2MzIn0sImlhdCI6MTU2MjQ4MzY2NywiZXhwIjoxNTY2MDgzNjY3fQ.2hO1REE2wgPT7viYUmFqS0eGPq8wUJpsdJPLD5Ys0TQ'
+                        'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWQyMmRlM2RiMWE2MGYxY2Q0OWE2YTgyIn0sImlhdCI6MTU2MzE2ODA5NywiZXhwIjoxNTY2NzY4MDk3fQ.ze6DO_4ngdHzulRFniEcXbdIHqjKFj4c-U6zbsNb0fI'
+               }
                     }
-                }
+
                 const body = JSON.stringify(order1)
 
-                const res = axios.post('/api/orders', body, config)
+                const res = await axios.post('/api/orders', body, config)
                 console.log(res.data)
             } catch (err) {
                 console.error(err.response.data)
@@ -45,7 +133,9 @@ const Order = () => {
     return (
         <Fragment>
         <h1>Orders page</h1>
-        <button onClick={myFunction}> Add Heavy Coat </button>
+        <button onClick={HeavyCoatadd}> Add Heavy Coat </button>
+        <button onClick={Jacketadd}> Add Jacket </button>
+        <button onClick={Trousersadd}> Add Trousers </button>
         <button onClick={onSubmit}> Submit </button>
         <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
