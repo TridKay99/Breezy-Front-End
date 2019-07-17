bfa1465214f0ff20f96e4380dd214a05c371d601
Tristan Kay <tridkay90@gmail.com>
2019-07-17 14:38:32 +1000

    changed routes file to add auth on admin

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import Routes from './Routes';
 
 
 class App extends React.Component {
+  state = {
+    auth: false
+  }
+
+  componentDidMount() {
+    // hit the backend
+    // check the token
+    // get the response
+    // if boolean is true in response set auth to be true
+    this.setState({
+      auth: true
+    })
+    // otherwise set auth to be false
+  }
   render () { 
-    return (
-    <Routes />
-    );
+    const { auth } = this.state
+    if (!this.state.auth) {
+      return null
+    } else {
+      return (
+        <Routes auth={auth} />
+      )
+    }
   }
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import './cssComponents/Profile.css'
+import Home from './Home'
 
 class Profile extends React.Component {
   state = {
@@ -20,7 +21,7 @@ class Profile extends React.Component {
     this.getUsers();
     const {users} = this.state
     if (!users) {
-      return <h1>Loading...</h1>
+      return <div class="spinner"></div>                 
     } else {
       const myuser = users.find((user) =>{
         if (user._id === "5d2c13b9700c640b08924761") {
@@ -31,6 +32,11 @@ class Profile extends React.Component {
       });
       if(myuser) {
         return (
+        <React.Fragment>
+        {/* <Nav /> */}
+        <div className="subNav">
+          <Link to={'/'}>Home</Link>
+        </div>
         <div className="profileContainer">
           <div className="userTitle">
             <h1>{myuser.fullname}'s Profile</h1>
@@ -39,10 +45,19 @@ class Profile extends React.Component {
             <div className="profileInfo">
               <p>Delivery Address:</p><h4>{myuser.address}</h4> 
               <p>Email:</p><h4>{myuser.user.email}</h4> 
-              <p>User id:</p><h4>{myuser.user._id}</h4> 
+              <p>User id:</p><h4>{myuser.user._id}</h4>
+              <p>Current Orders:</p>
+            </div>
+            <div class="profileOrdersContainer">
+              <div className="profileOrders">
+                  <p>ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE 
+                  ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE ORDERS GO HERE 
+                  </p>
+              </div>
             </div>
           </div>
         </div>
+        </React.Fragment>
         )
       } else {
         return <h1>comes here</h1> 
