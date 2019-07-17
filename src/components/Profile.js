55b8c0695256e6e27182f657182b51482b4c7941
Tristan Kay <tridkay90@gmail.com>
2019-07-17 12:02:53 +1000

    profile page + small amount of css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,39 +2,55 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import './cssComponents/Profile.css'
 
 class Profile extends React.Component {
   state = {
     users: null
   };
-  
-  async componentDidMount() {
+
+   async getUsers() {
     const response = await axios('http://localhost:5000/api/profile/users')
     this.setState({
       users: response.data
     })
-    const {users} = this.state
-    users.find((user) => {
-      if (user._id === "5d23ea2b68a4d31daa40cbaf") {
-        console.log(user)
-      }
-    })
   }
-
-
-
-  // In this render we ideally want basic information(Name, DOB, Date create,(IMAGE?, if no image, default image?))
-  // A link or a side box with the users pending/history of jobs thats been created. "DRY CLEANING COMING  UP"
 
   render() {
-    return(
-    <>
-      <h1>profile page</h1>
-      <button><Link to={'/orders'}>Make an Order</Link></button>
-    </>
-    )
+    this.getUsers();
+    const {users} = this.state
+    if (!users) {
+      return <h1>Loading...</h1>
+    } else {
+      const myuser = users.find((user) =>{
+        if (user._id === "5d2c13b9700c640b08924761") {
+          return user;
+        } else {
+          return null;
+        }
+      });
+      if(myuser) {
+        return (
+        <div className="profileContainer">
+          <div className="userTitle">
+            <h1>{myuser.fullname}'s Profile</h1>
+          </div>
+          <div className="profileInfoContainer">
+            <div className="profileInfo">
+              <p>Delivery Address:</p><h4>{myuser.address}</h4> 
+              <p>Email:</p><h4>{myuser.user.email}</h4> 
+              <p>User id:</p><h4>{myuser.user._id}</h4> 
+            </div>
+          </div>
+        </div>
+        )
+      } else {
+        return <h1>comes here</h1> 
+      }
+    }
   }
 }
+
 
 
 export default Profile
