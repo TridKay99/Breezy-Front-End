f45225587799383d6362d893fc628529f0b0e027
Tristan Kay <tridkay90@gmail.com>
2019-07-22 13:05:43 +1000

    functionality admin complete + some single order css

--- src/components/displayOrder.js
+++ src/components/displayOrder.js
@@ -8,36 +8,37 @@ class DisplayOrder extends React.Component {
     if (!this.props.order) {
       return null
     } else {
-      console.log(this.props.order)
       return (
       <div className="clothingContainer">
         <div className="clothingTitle">
           <h3>Email: {this.props.order.user.email}</h3>
           <h4>Address: {this.props.order.location}</h4>
         </div>
-        <div className="order">
-          <div className="insideOrderBox">
-            <h3>Order:</h3> {this.props.order.clothes.map((clothes, index) => {
-              if (clothes === "5d281b5a95643703ba61b8d1") {
-                return <p>Heavy Coat</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d2") {
-                return <p>Trousers</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d3") {
-                return <p>Jacket</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d4") {
-                return <p>Dress</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d5") {
-                return <p>Skirt</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d6") {
-                return <p>Blouse</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d7") {
-                return <p>Shirt</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d8") {
-                return <p>Tie</p>
-              } else if (clothes === "5d281b8d95643703ba61b8d9") {
-                return <p>Scarf</p>
-              }
-            })}
+        <div className="orderContainer">
+          <div className="insideOrderContainer">
+            <div className="order">
+              <h3>Order:</h3> {this.props.order.clothes.map((clothes, index) => {
+                if (clothes === "5d281b5a95643703ba61b8d1") {
+                  return <p key={index} >Heavy Coat $24.20</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d2") {
+                  return <p key={index}>Trousers $12.10</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d3") {
+                  return <p key={index}>Jacket $13.10</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d4") {
+                  return <p key={index}>Dress $22.00</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d5") {
+                  return <p key={index}>Skirt $12.10</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d6") {
+                  return <p key={index}>Blouse $12.10</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d7") {
+                  return <p key={index}>Shirt $5.00</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d8") {
+                  return <p key={index}>Tie $11.00</p>
+                } else if (clothes === "5d281b8d95643703ba61b8d9") {
+                  return <p key={index}>Scarf $12.20</p>
+                }
+              })}
+            </div>
           </div>
           <h4>Price: ${this.props.order.price.toFixed(2)}</h4>
         </div>
